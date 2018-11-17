78ff71e1874e4e29e1bf0907e606ee7f04b440fc
Fabian Beckmann <fb@phaseq.de>
2018-11-17 17:08:56 +0100

    python: read results asynchronously

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import subprocess
+import json
 
 XGE_LOCATION="target/release/xge"
 
@@ -17,9 +18,23 @@ print address_string
 #xge_lock = threading.Lock()
 #xge_lock.acquire()
 
-xge_client = threading.Thread(target=lambda: 
-	subprocess.call([XGE_LOCATION, "client", address_string])
-	)
+results_condition = threading.Condition()
+results = []
+
+def xge_thread():
+	proc = subprocess.Popen([XGE_LOCATION, "client", address_string], stdout=subprocess.PIPE)
+	while True:
+		line = proc.stdout.readline()
+		if line == '': break
+		line = line.split('mwt ')
+		if len(line) == 1: continue
+		result = json.loads(line[1])
+		results_condition.acquire()
+		results.append(result)
+		results_condition.notify()
+		results_condition.release()
+
+xge_client = threading.Thread(target=xge_thread)
 xge_client.start()
 
 print "thread started"
@@ -34,3 +49,9 @@ for i in range(20):
 	cs.send('["/Users/fabian/Dev/Rust/xge","sometitle","{}",'
 		'"python","-c","print \'hi\'"]\n'.format(i))
 cs.close()
+
+while xge_client.is_alive():
+	for result in results:
+		print "-->", result
+	with results_condition:
+		results_condition.wait(timeout=360)
